453240edf34a26f448f1f5f5159ee86220b7c557
Anil Yildiz <yildiz@stanford.edu>
2020-11-26 01:37:10 +0300

    minor changes

--- src/main.py
+++ src/main.py
@@ -10,7 +10,9 @@ def parse_arguments():
 
     parser.add_argument('--root_dir', type=str, default=os.getcwd(), help="Root directory to start traversing. Defaults to current working directory.")
     parser.add_argument('--delete_original', type=bool, default=True, help="Do you want to delete the original (large) file after compressing to archives?")
+    
     parser.add_argument('--partition_ext', type=str, default="7z", choices=["7z", "xz", "bzip2", "gzip", "tar", "zip", "wim"], help="Extension of the partitions. Recommended: 7z due to compression ratio and inter-OS compability.")
+    parser.add_argument('--cmds_into_7z', type=str, default="a", help="Commands to pass in to 7z.")
 
     # The two arguments below default to compressing files only if they are over 100 MB.
     parser.add_argument('--threshold_size', type=int, default=100, help="Max threshold of the original file size to split into archive. I.e. files with sizes below this arg are ignored.")
@@ -38,10 +40,7 @@ def is_over_threshold(f_full_dir, args):
         "m": 1e-6,
         "g": 1e-9
     }
-    if os.stat(f_full_dir).st_size * size_dict[args.threshold_size_unit] >= args.threshold_size:
-        return True
-    else:
-        return False
+    return os.stat(f_full_dir).st_size * size_dict[args.threshold_size_unit] >= args.threshold_size
 
 
 def traverse_root_dir(args):
@@ -51,7 +50,7 @@ def traverse_root_dir(args):
 
             if is_over_threshold(f_full_dir, args):
                 f_full_dir_noext, ext = os.path.splitext(f_full_dir)
-                subprocess.run(["7z", "-v" + str(args.partition_size) + args.partition_size_unit, "a", f_full_dir_noext + "." + ext[1:] + "." + args.partition_ext, f_full_dir])
+                subprocess.run(["7z", "-v" + str(args.partition_size) + args.partition_size_unit, args.cmds_into_7z, f_full_dir_noext + "." + ext[1:] + "." + args.partition_ext, f_full_dir])
                 
                 if args.delete_original:
                     os.remove(f_full_dir) 
