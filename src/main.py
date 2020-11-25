92c7f370bf0c0d07495d524078944d7426fee7d1
Anil Yildiz <yildiz@stanford.edu>
2020-11-26 01:20:24 +0300

    added reversing

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ def parse_arguments():
 
     parser.add_argument('--root_dir', type=str, default=os.getcwd(), help="Root directory to start traversing. Defaults to current working directory.")
     parser.add_argument('--delete_original', type=bool, default=True, help="Do you want to delete the original (large) file after compressing to archives?")
+    parser.add_argument('--partition_ext', type=str, default="7z", choices=["7z", "xz", "bzip2", "gzip", "tar", "zip", "wim"], help="Extension of the partitions. Recommended: 7z due to compression ratio and inter-OS compability.")
 
     # The two arguments below default to compressing files only if they are over 100 MB.
     parser.add_argument('--threshold_size', type=int, default=100, help="Max threshold of the original file size to split into archive. I.e. files with sizes below this arg are ignored.")
@@ -31,14 +32,12 @@ def check_7z_install():
 
 
 def is_over_threshold(f_full_dir, args):
-
     size_dict = {
         "b": 1e-0,
         "k": 1e-3,
         "m": 1e-6,
         "g": 1e-9
     }
-
     if os.stat(f_full_dir).st_size * size_dict[args.threshold_size_unit] >= args.threshold_size:
         return True
     else:
@@ -46,15 +45,13 @@ def is_over_threshold(f_full_dir, args):
 
 
 def traverse_root_dir(args):
-    for root, dirs, files in os.walk(args.root_dir):
-#        for d in dirs:
-#            print(os.path.join(root, d))
+    for root, _, files in os.walk(args.root_dir):
         for f in files:
             f_full_dir = os.path.join(root, f)
 
             if is_over_threshold(f_full_dir, args):
-                f_full_dir_noext, _ = os.path.splitext(f_full_dir)
-                subprocess.run(["7z", "-v" + str(args.partition_size) + args.partition_size_unit, "a", f_full_dir_noext + ".7z", f_full_dir])
+                f_full_dir_noext, ext = os.path.splitext(f_full_dir)
+                subprocess.run(["7z", "-v" + str(args.partition_size) + args.partition_size_unit, "a", f_full_dir_noext + "." + ext[1:] + "." + args.partition_ext, f_full_dir])
                 
                 if args.delete_original:
                     os.remove(f_full_dir) 
---
+++ src/reverse.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import shutil
+import subprocess
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='GitHub-ForceLargeFiles_reverse')
+
+    parser.add_argument('--root_dir', type=str, default=os.getcwd(), help="Root directory to start traversing. Defaults to current working directory.")
+    parser.add_argument('--delete_partitions', type=bool, default=True, help="Do you want to delete the partition archives after extracting the original files?")
+    
+    args = parser.parse_args()
+    return args
+
+
+def check_7z_install():
+    if shutil.which("7z"):
+        return True
+    else:
+    	sys.exit("ABORTED. You do not have 7z properly installed at this time. Make sure it is added to PATH.")
+
+
+def is_partition(f_full_dir):
+    return any(f_full_dir.endswith(ext) for ext in [".7z.001", ".xz.001", ".bzip2.001", ".gzip.001", ".tar.001", ".zip.001", ".wim.001"])
+
+
+def reverse_root_dir(args):
+    for root, _, files in os.walk(args.root_dir):
+        for f in files:
+            f_full_dir = os.path.join(root, f)
+
+            if is_partition(f_full_dir):
+                subprocess.run(["7z", "e", f_full_dir, "-o" + root])
+                
+                if args.delete_partitions:
+                    f_noext, _ = os.path.splitext(f)
+                    os.chdir(root)
+                    os.system("rm" + " \"" + f_noext + "\"*")
+
+
+if __name__ == '__main__':
+    check_7z_install()
+    reverse_root_dir(parse_arguments())
